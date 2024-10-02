b8a1a85695803f097ddfcbb2928aca64c21fc784
JulioPonce77 <jmunozp7@miumg.edu.gt>
2024-10-02 12:38:09 -0600

    Inicio de Sesion al abrir la página

--- Farmacia/urls.py
+++ Farmacia/urls.py
@@ -18,13 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
+from django.shortcuts import redirect
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include("Autenticacion.urls")),
+    path('auth/', include('Autenticacion.urls')),
     path('api/', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-    path('', include("Inicio.urls")),
+    path('', lambda request: redirect('login')),  
 ]
