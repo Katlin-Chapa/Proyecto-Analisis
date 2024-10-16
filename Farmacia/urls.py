de42ba26b7d9bf5b8fdbbf8ae7e6582cbb4ab047
Katlin-Chapa <kchapal@miumg.edu.gt>
2024-10-16 01:17:43 -0600

    Se arreglo

--- Farmacia/urls.py
+++ Farmacia/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('auth/', include('Autenticacion.urls')),
     path('api/', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-    path('', lambda request: redirect('login')),  
+    path('', include("Inicio.urls")),
 ]
