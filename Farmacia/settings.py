d7842c37257a3b187d976150ab22b2b8344e13fe
Katlin-Chapa <kchapal@miumg.edu.gt>
2024-10-23 07:50:26 -0600

    Configuracion

--- Farmacia/settings.py
+++ Farmacia/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'widget_tweaks', # Modificar formularios desde las plantillas.                       
+    'crispy_forms', # Facilita el renderizado de formularios con Bootstrap.
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'Farmacia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': ["templates", BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static/', BASE_DIR]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'   
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
