cef41aa949f37e1659bab4a0d06f16be1d6f8eee
Katlin-Chapa <kchapal@miumg.edu.gt>
2024-10-24 16:37:27 -0600

    models

--- Ventas/admin.py
+++ Ventas/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Factura, ArticuloVenta
+from .models import Factura, Venta
 
-# Registro de los modelos en el admin
-admin.site.register(Factura)
-admin.site.register(ArticuloVenta)
+# Configuración del modelo Factura para el admin
+class FacturaAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'nombre', 'telefono', 'correo_electronico', 'nit', 'direccion', 'tiempo')  # Añadí 'tiempo'
+    search_fields = ('numero', 'nombre')  # Añadir barra de búsqueda por número de factura y nombre
+    list_filter = ('tiempo',)  # Filtrar por fecha (tiempo)
+
+# Configuración del modelo Venta para el admin
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ('factura', 'stock', 'cantidad', 'precio_unitario', 'precio_total')  # Campos a mostrar
+    search_fields = ('factura__numero', 'stock__nombre')  # Añadir búsqueda por número de factura y nombre del stock
+    list_filter = ('factura__tiempo',)  # Filtrar por fecha de factura y por stock
+
+# Registrar los modelos en el admin
+admin.site.register(Factura, FacturaAdmin)
+admin.site.register(Venta, VentaAdmin)
